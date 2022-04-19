b72d3d02be6a05e956411f80c9684f3ad8b94ff7
Erick <erick.nahuelpaiva.001@gmail.com>
2022-04-19 11:59:38 -0300

    detallada la practica 1-6

--- algoritmo.js
+++ algoritmo.js
@@ -1,16 +1,19 @@
 /*
     Clase 8 del martes 
 
-    hoy vimos acciones primitivas
+    hoy vimos acciones primitivas y tipos de dato primitivos
 
 */
 
+//practica 1-6 B
 // defino una variable llamada "nombre variable" y la inicializo con un valor literal de "el tipo de dato que use"
 var palabra = "texto"; // tipo de dato string
 var numero = 2; // tipo de dato int
 var decimal = 3.4; // tipo de dato float
 var booleano = true; // tipo de dato boolean
-/* 
+
+
+// practica 1-6 A
 var respuesta = prompt("Ingrese su nombre", ["nombre"]);//escribe mediante el navegador
 
 console.log( respuesta );// escribe algo mediante la consola
@@ -18,9 +21,10 @@ console.log( respuesta );// escribe algo mediante la consola
 document.write(`
     <h4>Nombre: </4>
     <h4>${ respuesta }</4>   
-`); */
+`);
 
 
-if(0.4){
+//consulta resuelta
+/* if(0.1){ // valores decimales distintos de 0 igual los toma como verdadero
     console.log("otro mensaje");// escribi en una terminal
-}
+} */
