ae8d34a715a262c3dbc6028a252db7b338d6e32d
Erick <erick.nahuelpaiva.001@gmail.com>
2022-04-19 11:52:34 -0300

    pasos para crear repositorios  local y enlazar con repo online

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Clase 8
+
+- git init
+- git add .
+- git commit -m "comentario del commit"
+- (realizo el enlace con github y uso los comandos que proporciona)
--- algoritmo.js
+++ algoritmo.js
@@ -10,7 +10,7 @@ var palabra = "texto"; // tipo de dato string
 var numero = 2; // tipo de dato int
 var decimal = 3.4; // tipo de dato float
 var booleano = true; // tipo de dato boolean
-
+/* 
 var respuesta = prompt("Ingrese su nombre", ["nombre"]);//escribe mediante el navegador
 
 console.log( respuesta );// escribe algo mediante la consola
@@ -18,5 +18,9 @@ console.log( respuesta );// escribe algo mediante la consola
 document.write(`
     <h4>Nombre: </4>
     <h4>${ respuesta }</4>   
-`);
+`); */
 
+
+if(0.4){
+    console.log("otro mensaje");// escribi en una terminal
+}
