780d3e7314fc4b03908a8207b5fa92a8f9c1eb40
Erick <erick.nahuelpaiva.001@gmail.com>
2022-04-19 11:13:28 -0300

    practica 1-6

---
+++ algoritmo.js
@@ -0,0 +1,22 @@
+/*
+    Clase 8 del martes 
+
+    hoy vimos acciones primitivas
+
+*/
+
+// defino una variable llamada "nombre variable" y la inicializo con un valor literal de "el tipo de dato que use"
+var palabra = "texto"; // tipo de dato string
+var numero = 2; // tipo de dato int
+var decimal = 3.4; // tipo de dato float
+var booleano = true; // tipo de dato boolean
+
+var respuesta = prompt("Ingrese su nombre", ["nombre"]);//escribe mediante el navegador
+
+console.log( respuesta );// escribe algo mediante la consola
+
+document.write(`
+    <h4>Nombre: </4>
+    <h4>${ respuesta }</4>   
+`);
+
